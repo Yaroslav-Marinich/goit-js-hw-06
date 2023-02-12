cf9bfc203098fa2c2fde5d01c0ee20c1d7f2a6da
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-02-12 16:29:19 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value;
+  output.textContent = event.currentTarget.value.trim();
 });
 
 textInput.addEventListener("blur", () => {
