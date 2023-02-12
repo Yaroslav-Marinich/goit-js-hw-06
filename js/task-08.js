903e08623ad1d63aa406f5d40e669e31049c9070
Yaroslav-Marinich <ysmarinich@gmail.com>
2023-02-12 16:22:55 +0200

    end task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+// Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+// Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+const allData = [];
+
+document.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+    const data = {};
+    const { email, password } = event.target.elements;
+
+  if (
+    email.value === "" ||
+    password.value === ""
+  ) {
+    alert("Не заповнене поле");
+  } else {
+    data.email = email.value;
+    data.password = password.value;
+      console.log(data);
+      allData.push(data);
+  }
+    email.value = "";
+    password.value = "";
+    console.log(allData);
+});
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+document.querySelector(".change-color").addEventListener("click", (event) => {
+  const hexColor = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = hexColor;
+  document.querySelector(".color").textContent = hexColor
+});
+
